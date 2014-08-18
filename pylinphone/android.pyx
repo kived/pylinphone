bb9e977071ef869e9a6a236c9b887b0b2f4a2428
Ryan Pessa <ryan@essential-elements.net>
2014-08-18 14:20:30 -0500

    fix jnienv

--- pylinphone/android.pyx
+++ pylinphone/android.pyx
@@ -2,17 +2,18 @@ include "config.pxi"
 
 IF ANDROID:
 	
-	from jnius.jnius cimport get_jnienv
-	
 	cdef extern from "jvm.h":
 		cdef struct JavaVM
 		cdef struct JNIEnv
+	
+	cdef extern JNIEnv* SDL_ANDROID_GetJNIEnv()
 	
 	cdef extern from "mediastreamer2/msjava.h":
 		void ms_set_jvm(JavaVM* vm)
 	
 	
 	def init_android():
-		cdef JNIEnv* jnienv = get_jnienv()
-		cdef JavaVM* jvm = jnienv.GetJavaVM()
+		cdef JavaVM* jvm
+		cdef JNIEnv* jnienv = SDL_ANDROID_GetJNIEnv()
+		jnienv[0].GetJavaVM(jnienv, &jvm)
 		ms_set_jvm(jvm)
